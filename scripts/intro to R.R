5338ef980c543d259e5944651548e5175269b2ee
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-08-28 20:12:14 -0400

    updates

---
+++ scripts/intro to R.R
@@ -0,0 +1,8 @@
+# install a package
+install.packages('devtools')
+devtools::install_github("brooke-watson/BRRR")
+# load a library
+library(BRRR)
+# run a function from the BRRR package
+skrrrahh(0)
+skrrrahh_list()
