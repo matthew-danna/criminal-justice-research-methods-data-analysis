77965df1f60dbe7a3b5442db72947f637f784574
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-10-10 20:51:15 -0400

    Update crim 515 project 02 2023 10 10.R

--- scripts/crim 515 project 02 2023 10 10.R
+++ scripts/crim 515 project 02 2023 10 10.R
@@ -1,10 +1,63 @@
-# Libaries
+# Libraries
 library(gtrendsR)
+library(tidyverse)
 
 # Step 1: Data
+## Police Shootings
 wapo.data <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv", 
                       stringsAsFactors = FALSE)
 
+## Google Trends
 today <- as.character(Sys.Date())
 time.range <- paste("2015-01-01", today, sep = " ")
 trends1 <- gtrends("police shooting", geo = "US", time = time.range)
+plot(trends1)
+
+# Step 2
+wapo.data$date <- as.Date(wapo.data$date)
+wapo.data$month <- substr(wapo.data$date, 6, 7)
+wapo.data$year <- substr(wapo.data$date,0,4)
+wapo.data$yearmonth <- paste(wapo.data$year, wapo.data$month, sep = "-")
+
+unique(wapo.data$race)
+wapo.data$race <- gsub("B;H", "O", wapo.data$race)
+
+# Step 3
+# single variable summary tables
+wapo.race <- wapo.data %>%
+  group_by(race) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+  
+wapo.year <- wapo.data %>%
+  group_by(year) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+wapo.state <- wapo.data %>%
+  group_by(state) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+wapo.armed <- wapo.data %>%
+  group_by(armed_with) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+wapo.mental <- wapo.data %>%
+  group_by(was_mental_illness_related) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+# multiple variable summary tables
+wapo.race.armed <- wapo.data %>%
+  group_by(race, armed_with) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+wapo.race.mental <- wapo.data %>%
+  group_by(race, was_mental_illness_related) %>%
+  summarise(count = n()) %>%
+  mutate(pct = round(count/sum(count)*100,2))
+
+
