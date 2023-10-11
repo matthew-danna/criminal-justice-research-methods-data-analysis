f446ed19bf2a111fa84a9b85b0a53e0c68a84122
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-10-10 20:19:40 -0400

    updates

--- scripts/crim 515 project 02 2023 10 10.R
+++ scripts/crim 515 project 02 2023 10 10.R
@@ -1,1 +1,10 @@
-# 
+# Libaries
+library(gtrendsR)
+
+# Step 1: Data
+wapo.data <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv", 
+                      stringsAsFactors = FALSE)
+
+today <- as.character(Sys.Date())
+time.range <- paste("2015-01-01", today, sep = " ")
+trends1 <- gtrends("police shooting", geo = "US", time = time.range)
