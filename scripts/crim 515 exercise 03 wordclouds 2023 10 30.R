cb3e77b24ab7845634f31b43bcfb9d71b0de28a1
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-10-30 19:51:29 -0400

    Create crim 515 exercise 03 wordclouds 2023 10 30.R

---
+++ scripts/crim 515 exercise 03 wordclouds 2023 10 30.R
@@ -0,0 +1,16 @@
+### Step 0: packages
+install.packages('pdftools')
+install.packages('tidytext')
+install.packages('tidyverse')
+install.packages('tokenizers')
+install.packages('wordcloud')
+install.packages('wordcloud2')
+install.packages('tm')
+
+library(pdftools)
+library(tidyverse)
+library(tidytext)
+library(tokenizers)
+library(wordcloud)
+library(wordcloud2)
+
