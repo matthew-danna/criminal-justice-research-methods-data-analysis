102090f459b953a47bce2c74e30e97436216ced0
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-11-06 21:35:00 -0500

    Update crim 515 project 03 2023 11 06.R

--- scripts/crim 515 project 03 2023 11 06.R
+++ scripts/crim 515 project 03 2023 11 06.R
@@ -35,7 +35,7 @@ library(BRRR)
 ### Step 2: DATA
 setwd("C:/Users/USERNAME HERE/Desktop/research articles") #Windows
 setwd("/Users/USERNAME HERE/Desktop/research articles") #Mac
-setwd("/Users/matthewdanna/Downloads/research articles") #Mac
+#setwd("/Users/matthewdanna/Downloads/research articles") #Mac
 skrrrahh(0)
 
 ### Step 3: CLEAN DATA
@@ -49,7 +49,7 @@ for (file in file.list) {
   my.data <- rbind(tmp.data, my.data)
 }
 
-# STEMMING WORDS
+# UNNEST & STEMMING WORDS
 my.data$file.ID <- seq.int(nrow(my.data))
 my.data$text <- as.character(my.data$text)
 my.words <- my.data %>% unnest_tokens(word, text)
@@ -90,7 +90,8 @@ summary.nrc <- my.nrc %>%
   summarise(count = n()) %>%
   mutate(PCT = round(count/sum(count)*100,2))
 
-my.sentiment <- my.afinn
+my.sentiment1 <- my.afinn
+my.sentiment2 <- my.bing
 
 ## Word Counts
 # counts of unique usable words
@@ -99,10 +100,86 @@ counts.my.clean.words <- my.clean.words %>%
   mutate(PCT = round(n/sum(n)*100,2))
 
 # counts of words with sentiment
-counts.my.sentiment <- my.sentiment %>% 
+counts.my.sentiment <- my.sentiment1 %>% 
   count(word, sort = TRUE) %>%
   mutate(PCT = round(n/sum(n)*100,2))
 
-counts.sentiments <- my.sentiment %>% count(sentiment, sort = TRUE)
+# counts of sentiments
+counts.sentiments <- my.sentiment1 %>% count(value, sort = TRUE)
+counts.sentiments <- my.sentiment2 %>% count(sentiment, sort = TRUE)
+
+# counts of words with sentiment for a specific article
+counts.koper1995 <- my.sentiment1 %>% 
+  subset(file == "Koper 1995.pdf") %>% 
+  count(word, value, sort = TRUE) %>%
+  mutate(PCT = round(n/sum(n)*100,2))
+
+# to get a full list of all the articles available, run this:
+file.list
+
+top.my.clean.words <- subset(counts.my.clean.words, n >= quantile(counts.my.clean.words$n, 0.999))
+
+## Word Pairs
+# for entire corpus
+wordpairs.my.words <- my.clean.words %>% pairwise_count(word, file.ID, sort = TRUE)
+
+# for a specific article
+wordpairs.koper1995 <- my.clean.words %>% 
+  subset(file == 'Koper 1995.pdf') %>% 
+  pairwise_count(word, file.ID, sort = TRUE)
+
+# for most frequently occurring word pairs
+wordpairs.top <- subset(wordpairs.my.words, n >= quantile(wordpairs.my.words$n, 0.99999))
+
+### Step 5: VISUALIZE
+# bar graph
+ggplot(top.my.clean.words, aes(x=word, y=n, fill=n)) + 
+  geom_bar(stat = "identity") + 
+  coord_flip()
+
+# wordcloud
+wordcloud(my.clean.words$word, max.words = 1000)
+
+# network chart
+set.seed(611)
+pairs.plot <- wordpairs.my.words %>%
+  filter(n >= 350) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "steelblue") +
+  ggtitle("Word Pairs! For combinations greater than 350") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+pairs.plot
+
+# all the visuals for one article of interest
+my.clean.article <- subset(my.clean.words, file == "d_anna 2016.pdf") #yep we’re testing mine
+counts.my.clean.article <- my.clean.article %>% count(word, sort = TRUE)
+top.my.clean.article <- subset(counts.my.clean.article, n >= quantile(counts.my.clean.article$n, 0.95))
+wordpairs.article <- my.clean.article %>% pairwise_count(word, file.ID, sort = TRUE)
+ggplot(top.my.clean.article, aes(x=word, y=n, fill=n)) + geom_bar(stat = "identity") + coord_flip()
+set.seed(611)
+pairs.plot.article <- wordpairs.article %>%
+  filter(n >= 5)               %>%
+  graph_from_data_frame()        %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "steelblue") +
+  ggtitle("Word Pairs for one article") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
+pairs.plot.article
+
+# Topic Modeling
+
+
+
 
 
