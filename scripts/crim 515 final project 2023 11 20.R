afff999980bd4d2ef9bc2432e8dad8da54a42343
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-11-20 20:57:43 -0500

    updates

--- scripts/crim 515 final project 2023 11 20.R
+++ scripts/crim 515 final project 2023 11 20.R
@@ -19,7 +19,35 @@ calls.subset1 <- subset(calls, calls$type == 'MISC')
 calls.subset2 <- subset(calls, calls$type == 'MISC' |
                          calls$type == 'SERVICE')
 
+# for more than two calls:
+calls.group <- c("MISC", "SERVICE", "UNKNOWN", "Special Detail")
+
+calls.subset3 <- calls %>%
+  filter(calls$type %in% calls.group)
+
 ##### 3: Summarize your events by a time measure
+calls.subset3$date <- as.Date(calls.subset3$date)
+calls.subset3$week <- week(ymd(calls.subset3$date))
+calls.subset3$month <- substr(calls.subset3$date, 6,7)
+
+# for seasons:
+calls.summer <- subset(calls.subset3, calls.subset3$month == '06' |
+                         calls.subset3$month == '07' |
+                         calls.subset3$month == '08')
+calls.winter <- subset(calls.subset3, calls.subset3$month == '11' |
+                       calls.subset3$month == '12' |
+                         calls.subset3$month == '01')
+calls.spring <- subset(calls.subset3, calls.subset3$month == '02' |
+                         calls.subset3$month == '03' |
+                         calls.subset3$month == '04' |
+                         calls.subset3$month == '05')
+calls.fall <- subset(calls.subset3, calls.subset3$month == '09' |
+                         calls.subset3$month == '10')
+calls.summer$season <- "SUMMER"
+calls.winter$season <- "WINTER"
+calls.spring$season <- "SPRING"
+calls.fall$season <- "FALL"
+calls.seasons <- rbind(calls.summer, calls.winter, calls.spring, calls.fall)
 
 ##### 4: Identify trends over time
 
