1a6abff3b0db5541a6cc2763bff5219a553123ea
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-11-20 20:18:11 -0500

    new files

---
+++ scripts/crim 515 final project 2023 11 20.R
@@ -0,0 +1,13 @@
+# 1: Read the file(s) into RStudio
+
+# 2: Select/subset any specific categories
+
+# 3: Summarize your events by a time measure
+
+# 4: Identify trends over time
+
+# 5: Determine a break-point
+
+# 6: Calculate a t test to identify statistically significant changes
+
+# 7: Visualize the findings
