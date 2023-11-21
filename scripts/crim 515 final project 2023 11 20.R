2c24f296b9150187c46d855d1c32fb7926696fda
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-11-20 20:39:25 -0500

    Update crim 515 final project 2023 11 20.R

--- scripts/crim 515 final project 2023 11 20.R
+++ scripts/crim 515 final project 2023 11 20.R
@@ -1,14 +1,30 @@
-# 1: Read the file(s) into RStudio
+##### 0: Libraries
+library(tidyverse)
+
+##### 1: Read the file(s) into RStudio
 calls <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", "1gRoL7lZlwm7sreA5F9WbPoH5su4n4iGS"))
 
-# 2: Select/subset any specific categories
+##### 2: Select/subset any specific categories
+unique(calls$type)
 
-# 3: Summarize your events by a time measure
+calls.summary <- calls %>%
+  group_by(type) %>%
+  summarise(COUNT = n()) %>%
+  mutate(PCT = round(COUNT/sum(COUNT),2))
 
-# 4: Identify trends over time
+# for a single call type:
+calls.subset1 <- subset(calls, calls$type == 'MISC')
 
-# 5: Determine a break-point
+# for two call types:
+calls.subset2 <- subset(calls, calls$type == 'MISC' |
+                         calls$type == 'SERVICE')
 
-# 6: Calculate a t test to identify statistically significant changes
+##### 3: Summarize your events by a time measure
 
-# 7: Visualize the findings
+##### 4: Identify trends over time
+
+##### 5: Determine a break-point
+
+##### 6: Calculate a t test to identify statistically significant changes
+
+##### 7: Visualize the findings
