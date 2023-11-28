b23a9252bb6bfa9a706a3c057cf9746dca00ac0e
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-11-27 19:46:31 -0500

    updates

---
+++ scripts/crim 515 final project 2023 11 27.R
@@ -0,0 +1,10 @@
+0. Libraries
+1. Get data
+2. Add date formats (week, season, quarter)
+3. Subset
+4. Groups (presidents, COVID, plus 1)
+5. Choose time measure
+6. Run t tests
+7. Pick best on
+## Find best relationship. 
+## Map hotspots and find changes in areas
